be5d830eac2dc0792049303d3fef765edf5c5f7f
Damian404 <sgt.curren@gmail.com>
2019-10-01 15:30:56 +0100

    Frontend skeleton

---
+++ Chat-app-frontend/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import {Route} from 'react-router';
+import Home from './components/Home';
+
+const App = () => (
+    <div>
+        <main>
+            
+        </main>
+    </div>
+)
+
+export default App;
